79a904d0e5cc610b36f5789f271cd3f8d70d0a53
cheerryio <2221619190@qq.com>
2021-03-22 10:44:51 -0500

    a

---
+++ src/pages/support/sleep/sleep.vue
@@ -0,0 +1,57 @@
+<template>
+	<view>
+		<view>
+			<text>时间：{{date.getMonth() + 1}} - {{date.getDate()}}</text>
+		</view>
+		<view>
+			<u-input v-model="value" type="select" :border="true" @click="show = true" />
+			<u-action-sheet :list="sleepHoursList" v-model="show" @click="sleepHoursActionSheetCallback"></u-action-sheet>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				loading:false,
+				show:false,
+				value:'',
+				date:'',
+				sleepHoursList:[{
+					text:"超过9小时"
+				},{
+					text:"6-9小时"
+				},{
+					text:"小于6小时"
+				}]
+			}
+		},
+
+		onLoad:function(){
+			this.loading=true;
+			uni.showLoading({
+				title:"加载中"
+			})
+
+			this.init();
+
+			uni.hideLoading();
+			this.loading=false;
+		},
+
+		methods: {
+			init(){
+				this.date=new Date();
+
+			},
+			sleepHoursActionSheetCallback(index){
+				this.value=this.sleepHoursList[index].text;
+			},
+		}
+	}
+</script>
+
+<style>
+
+</style>
--- src/pages/support/support.vue
+++ src/pages/support/support.vue
@@ -1,11 +1,13 @@
 <template>
 	<view class="container">
 		<uni-grid :column="3" :square="false">
-			<uni-grid-item v-for="(sItem,sIndex) in items" :key="`item-box-${sIndex}`">
-				<view class="item-box">
-					<image :src="sItem.icon"></image>
-					<text>{{sItem.title}}</text>
-				</view>
+			<uni-grid-item v-for="(sItem,sIndex) in items" :key="sIndex">
+					<navigator :url="sItem.link">
+						<view class="item-box">
+							<image :src="sItem.icon"></image>
+							<text>{{sItem.title}}</text>
+						</view>
+					</navigator>
 			</uni-grid-item>
 		</uni-grid>
 	</view>
@@ -18,24 +20,34 @@
 			return {
 				items:[{
 					title:"睡眠记录",
-					icon:"../../static/icon/shuimian.png"
+					icon:"../../static/icon/shuimian.png",
+					link:"/pages/support/sleep/sleep"
 				},{
 					title:"运动记录",
-					icon:"../../static/icon/yundong_1.png"
+					icon:"../../static/icon/yundong_1.png",
+					link:"/pages/support/exercise/exercise"
 				},{
 					title:"血糖记录",
-					icon:"../../static/icon/xietang.png"
+					icon:"../../static/icon/xietang.png",
+					link:"/pages/support/bloodsugar/bloodsugar"
 				},{
 					title:"饮食记录",
-					icon:"../../static/icon/yinshi.png"
+					icon:"../../static/icon/yinshi.png",
+					link:"/"
 				},{
 					title:"心情记录",
-					icon:"../../static/icon/xinqingyiban-yuan.png"
+					icon:"../../static/icon/xinqingyiban-yuan.png",
+					link:"/pages/support/mood/mood"
 				},{
 					title:"药物记录",
-					icon:"../../static/icon/yao.png"
+					icon:"../../static/icon/yao.png",
+					link:"/pages/support/drug/drug"
 				}]
 			}
+		},
+
+		methods:{
+
 		}
 	}
 </script>
